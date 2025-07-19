591d510f9d2ed82ea812cc66965ab8cc06ed1ca4
apekshas698 <apekshas729@gmail.com>
2025-07-19 12:53:26 +0530

    Cleaned up old root files after moving to frontend/

--- src/Components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children }) => {
-  return <div className="rounded-xl shadow-md p-4 bg-white">{children}</div>;
-};
-
-export const CardContent = ({ children }) => {
-  return <div className="mt-2">{children}</div>;
-};
